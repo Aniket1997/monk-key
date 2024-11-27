6f0590e5d81abd15ea6b8150b5fa9d15c608589d
Aniket1997 <16riju@gmail.com>
2024-11-28 00:00:32 +0530

    Talking ai svg added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from './pages/Login'
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard"
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
         </Routes>
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TalkingAI from '../component/TalkingA'
+
+const Dashboard : React.FC = () => {
+  return (
+    <>
+    <div>Dashboard</div>
+    <TalkingAI/>
+    </>
+  )
+}
+
+export default Dashboard
